9a7997dd16a1edb6c24c3d3ee26a378e2586e86b
olikraus <olikraus@localhost>
2014-01-06 15:32:32 +0100

    font array

--- tools/font/build/do_combine.sh
+++ tools/font/build/do_combine.sh
@@ -28,7 +28,7 @@ fgrep ucg_fntpgm_uint8_t ucg_vector_font_data.c | sed -e 's/^\([^[]*\).[0-9]*\([
 # generate array with all fonts
 
 echo "#include \"ucg.h\"" > ucg_font_array.c
-echo "ucg_fntpgm_uint8_t *ucg_font_array[] = {" >> ucg_font_array.c
-cut -d " " -f 4 font_data.h | sed -e 's|\[\]|,|' >> ucg_font_array.c
+echo "const ucg_fntpgm_uint8_t *ucg_font_array[] = {" >> ucg_font_array.c
+cut -d " " -f 4 ucg_font_data.h | sed -e 's|\[\]|,|' >> ucg_font_array.c
 echo "NULL" >> ucg_font_array.c
 echo "};" >> ucg_font_array.c
