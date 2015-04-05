2ad8c8e736d06e12d5676dc3e34e34d7e5fa41bf
olikraus <olikraus@gmail.com>
2015-04-05 10:53:26 +0200

    bugfix & overview pic

--- tools/font/bdfconv/main.c
+++ tools/font/bdfconv/main.c
@@ -78,6 +78,7 @@ void help(void)
   printf("-l <margin>   Overview picture: Set left margin\n");
   printf("-d <fontname> Overview picture: Font for description\n");
   printf("-a            Overview picture: Additional font information (background, orange&blue dot)\n");
+  printf("-t            Overview picture: Test string (Woven silk pyjamas exchanged for blue quartz.)\n");
   printf("\n");
 
   printf("map := <mapcmd> { \",\" <mapcmd> }\n");
@@ -103,6 +104,7 @@ void help(void)
 unsigned long left_margin = 1;
 unsigned long build_bbx_mode = 0;
 int font_picture_extra_info = 0;
+int font_picture_test_string = 0;
 
 /*================================================*/
 
@@ -222,7 +224,11 @@ void tga_draw_font(unsigned y, const char *fontname, bf_t *bf_desc_font, bf_t *b
   
   //tga_draw_string(0, y, "Woven silk pyjamas exchanged for blue quartz", 1, xmax);
   //y += tga_get_char_height()+1;
-  tga_draw_string(left_margin, y, "Woven silk pyjamas exchanged for blue quartz.", 0, xmax);
+  if ( font_picture_test_string != 0 )
+  {
+    tga_draw_string(left_margin, y, "Woven silk pyjamas exchanged for blue quartz.", 0, xmax);
+    y +=  tga_get_line_height(bf_desc_font, bf)+1;
+  }
 }
 
 
@@ -263,6 +269,10 @@ int main(int argc, char **argv)
     {
       font_picture_extra_info = 1;
     }
+    else if ( is_arg(&argv, 't') != 0 )
+    {
+      font_picture_test_string = 1;
+    }
     else if ( get_num_arg(&argv, 'b', &build_bbx_mode) != 0 )
     {
     }
@@ -300,40 +310,14 @@ int main(int argc, char **argv)
 
   
   bf = bf_OpenFromFile(bdf_filename, is_verbose, build_bbx_mode, map_str);
-  //bf = bf_OpenFromFile(bdf_filename, is_verbose, BDF_BBX_MODE_MINIMAL, map_str);
-  //bf = bf_OpenFromFile(bdf_filename, is_verbose, BDF_BBX_MODE_MAX, map_str);
-  //bf = bf_OpenFromFile(bdf_filename, is_verbose, BDF_BBX_MODE_HEIGHT, map_str);
   
   if ( bf == NULL )
   {
-    //bf_Error(bf, "Error with file '%s'", bdf_filename);
-
     exit(1);
   }
   
-  //bf_ShowAllGlyphs(bf, &(bf->max));
-
   tga_init(800, 600);
-  //tga_set_pixel(1, 1, 255,0,0);
-
-  tga_set_font(bf->target_data);
-  //tga_draw_glyph(10, 18, ' ');
-
-  //tga_draw_font_line(50, 64, bf_desc_font, bf);
   tga_draw_font(0, bdf_filename, bf_desc_font, bf);
-  
-  /*
-  tga_draw_glyph(40, 18, 'B');
-
-  tga_draw_glyph(10, 50, bf->enc_x);
-  tga_draw_glyph(10+30, 50, bf->enc_y);
-  tga_draw_glyph(10+30+30, 50, bf->enc_w);
-  tga_draw_glyph(10+30+30+30, 50, bf->enc_h);
-  tga_draw_glyph(10+30+30+30+30, 50, 'E');
-  */
-  //tga_draw_string(10,82,"BjAjQ QnBmj");
-  
-  
   tga_save("bdf.tga");
 
 
