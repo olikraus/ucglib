f7332b0e7c00142a77713384b2dad7559e3f6e61
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2020-08-29 18:09:52 +0100

    Add L90FX&L90SE

--- csrc/ucg_dev_st7789.c
+++ csrc/ucg_dev_st7789.c
@@ -15,7 +15,6 @@
 #define ST7789_COLMOD_COLOR_18BIT (1<<2|1<<1)  // 0x06
 #define ST7789_COLMOD_COLOR_READ (ST7789_COLMOD_RGB_262K|ST7789_COLMOD_COLOR_18BIT)
 #define ST7789_COLMOD_COLOR_16M (1<<2|1<<1|1)  // 0x07
-#define ST7789_MADCTL 0x36
 #define ST7789_CASET 0x2A
 #define ST7789_RASET 0x2B
 #define ST7789_INVOFF 0x20
@@ -24,6 +23,18 @@
 #define ST7789_NORON 0x13
 #define ST7789_RAMWR 0x2C
 #define ST7789_MADCTL 0x36
+#define ST7789_MADCTL_PAGE_ADDRESS_ORDER_TOP_TO_BOTTOM 0
+#define ST7789_MADCTL_PAGE_ADDRESS_ORDER_BOTTOM_TO_TOP (1<<7)
+#define ST7789_MADCTL_COLUMN_ADDRESS_ORDER_LEFT_TO_RIGHT 0
+#define ST7789_MADCTL_COLUMN_ADDRESS_ORDER_RIGHT_TO_LEFT (1<<6)
+#define ST7789_MADCTL_PAGE_COLUMN_ORDER_NORMAL 0
+#define ST7789_MADCTL_PAGE_COLUMN_ORDER_REVERSE (1<<5)
+#define ST7789_MADCTL_LINE_ADDRESS_ORDER_LCD_REFRESH_TOP_TO_BOTTOM 0
+#define ST7789_MADCTL_LINE_ADDRESS_ORDER_LCD_REFRESH_BOTTOM_TO_TOP (1<<4)
+#define ST7789_MADCTL_ORDER_RGB 0
+#define ST7789_MADCTL_ORDER_BGR (1<<3)
+#define ST7789_MADCTL_DISPLAY_DATA_LATCH_ORDER_LCD_REFRESH_LEFT_TO_RIGHT 0
+#define ST7789_MADCTL_DISPLAY_DATA_LATCH_ORDER_LCD_REFRESH_RIGHT_TO_LEFT 1
 
 #define ST7789_RESX_TRW_MS 10 // Reset pulse duration 
 #define ST7789_RESX_TRT_MS 120 // Reset cancel
@@ -39,7 +50,6 @@ static const ucg_pgm_uint8_t ucg_st7789_16x240x240_init_seq[] = {
   UCG_RST(1),
   UCG_DLY_MS(ST7789_RESX_TRT_MS),
 
-
   // Software reset
   UCG_CS(0),
   UCG_C10(ST7789_SWRESET),
@@ -54,6 +64,17 @@ static const ucg_pgm_uint8_t ucg_st7789_16x240x240_init_seq[] = {
 
   // Set color mode
   UCG_C11(ST7789_COLMOD, ST7789_COLMOD_COLOR_16BIT_WRITE),
+
+  // Memory Data Access Control
+  UCG_C11(
+    ST7789_MADCTL,
+    ST7789_MADCTL_PAGE_ADDRESS_ORDER_TOP_TO_BOTTOM |
+    ST7789_MADCTL_COLUMN_ADDRESS_ORDER_LEFT_TO_RIGHT |
+    ST7789_MADCTL_PAGE_COLUMN_ORDER_NORMAL |
+    ST7789_MADCTL_LINE_ADDRESS_ORDER_LCD_REFRESH_TOP_TO_BOTTOM |
+    ST7789_MADCTL_ORDER_RGB |
+    ST7789_MADCTL_DISPLAY_DATA_LATCH_ORDER_LCD_REFRESH_LEFT_TO_RIGHT
+  ),
 
   // Display inversion in practice works the opposite of what the datasheet
   // says: on is off and off is on.
@@ -77,14 +98,109 @@ static void send_cmd(ucg_t *ucg, uint8_t cmd) {
   ucg_com_SendString(ucg, 1, &cmd);
 }
 
-static void send_data(ucg_t *ucg, uint16_t count, uint8_t *data) {
+static void send_cmd_data(ucg_t *ucg, uint8_t cmd, uint16_t count, uint8_t *data) {
+  send_cmd(ucg, cmd);
   ucg_com_SetCDLineStatus(ucg, (ucg->com_cfg_cd)&1);
   ucg_com_SendString(ucg, count, data);
 }
 
-static void send_cmd_data(ucg_t *ucg, uint8_t cmd, uint16_t count, uint8_t *data) {
-  send_cmd(ucg, cmd);
-  send_data(ucg, count, data);
+static void set_column_address(ucg_t *ucg, uint16_t x_start, uint16_t x_end) {
+  uint8_t buff[4];
+
+  buff[0] = (x_start&0xFF00)>>8;
+  buff[1] = x_start&0xFF;
+  buff[2] = (x_end&0xFF00)>>8;
+  buff[3] = x_end&0xFF;
+
+  send_cmd_data(ucg, ST7789_CASET, 4, buff);
+}
+
+static void set_row_address(ucg_t *ucg, uint16_t y_start, uint16_t y_end) {
+  uint8_t buff[4];
+
+  buff[0] = (y_start&0xFF00)>>8;
+  buff[1] = y_start&0xFF;
+  buff[2] = (y_end&0xFF00)>>8;
+  buff[3] = y_end&0xFF;
+
+  send_cmd_data(ucg, ST7789_RASET, 4, buff);
+}
+
+static void set_16bit_color(ucg_t *ucg, uint8_t buff[2]) {
+  uint8_t r, g, b;
+
+  // 16 bit: RRRRRGGG GGGBBBBB
+  buff[0] = 0;
+  buff[1] = 0;
+  r = ucg->arg.pixel.rgb.color[0]>>(8-5);
+  buff[0] |= r << 3;
+  g = ucg->arg.pixel.rgb.color[1]>>(8-6);
+  buff[0] |= g>>3;
+  buff[1] |= g<<5;
+  b = ucg->arg.pixel.rgb.color[2]>>(8-5);
+  buff[1] |= b&0x1F;
+}
+
+#include <libopencm3/stm32/spi.h>
+
+static void handle_l90fx(ucg_t *ucg) {
+  ucg_int_t dx=0, dy=0;
+  uint8_t madctl_data=0;
+  uint8_t buff[2];
+
+  if ( ucg_clip_l90fx(ucg) == 0 )
+    return;
+
+  switch(ucg->arg.dir) {
+    case 0:
+      dx = 1;
+      dy = 0;
+      madctl_data = ST7789_MADCTL_COLUMN_ADDRESS_ORDER_RIGHT_TO_LEFT;
+      break;
+    case 1:
+      dx = 0;
+      dy = 1;
+      madctl_data = ST7789_MADCTL_PAGE_ADDRESS_ORDER_TOP_TO_BOTTOM;
+      break;
+    case 2:
+      dx = -1;
+      dy = 0;
+      madctl_data = ST7789_MADCTL_COLUMN_ADDRESS_ORDER_LEFT_TO_RIGHT;
+      break;
+    case 3:
+      dx = 0;
+      dy = -1;
+      madctl_data = ST7789_MADCTL_PAGE_ADDRESS_ORDER_BOTTOM_TO_TOP;
+      break;
+  }
+
+  ucg_com_SetCSLineStatus(ucg, 0);
+
+  send_cmd_data(ucg, ST7789_MADCTL, 1, &madctl_data);
+
+  set_column_address(
+    ucg,
+    ucg->arg.pixel.pos.x, // x_start
+    ucg->arg.pixel.pos.x + (ucg->arg.len * dx) // x_end
+  );
+  set_row_address(
+    ucg,
+    ucg->arg.pixel.pos.y, // y_start
+    ucg->arg.pixel.pos.y + (ucg->arg.len * dy) // y_end
+  );
+
+  send_cmd(ucg, ST7789_RAMWR);
+  ucg_com_SetCDLineStatus(ucg, (ucg->com_cfg_cd)&1);
+
+  set_16bit_color(ucg, buff);
+
+  for(ucg_int_t i = 0; i < ucg->arg.len; i++ ) {
+    ucg_com_SendString(ucg, 2, buff);
+    ucg->arg.pixel.pos.x+=dx;
+    ucg->arg.pixel.pos.y+=dy;
+  }
+
+  ucg_com_SetCSLineStatus(ucg, 1);
 }
 
 ucg_int_t ucg_dev_st7789_16x240x240(ucg_t *ucg, ucg_int_t msg, void *data) {
@@ -103,50 +219,28 @@ ucg_int_t ucg_dev_st7789_16x240x240(ucg_t *ucg, ucg_int_t msg, void *data) {
       return 1;
     case UCG_MSG_DRAW_PIXEL:
       if ( ucg_clip_is_pixel_visible(ucg) !=0 )  {
-        uint8_t r, g, b;
-        uint8_t buff[4];
-        uint16_t addr;
+        uint8_t buff[2];
 
         ucg_com_SetCSLineStatus(ucg, 0);
 
-         // Column addr set
-        addr = ucg->arg.pixel.pos.x;
-        buff[0] = (addr&0xFF00)>>8; // X start high bits
-        buff[1] = addr&0xFF; // X start low bits
-        buff[2] = buff[0]; // X end high bits
-        buff[3] = buff[1]; // X end low bits
-        send_cmd_data(ucg, ST7789_CASET, 4, buff);
-
-         // Row addr set
-        addr = ucg->arg.pixel.pos.y;
-        buff[0] = (addr&0xFF00)>>8; // X start high bits
-        buff[1] = addr&0xFF; // X start low bits
-        buff[2] = buff[0]; // X end high bits
-        buff[3] = buff[1]; // X end low bits
-        send_cmd_data(ucg, ST7789_RASET, 4, buff);
+        set_column_address(ucg, ucg->arg.pixel.pos.x, ucg->arg.pixel.pos.x);
+        set_row_address(ucg, ucg->arg.pixel.pos.y, ucg->arg.pixel.pos.y);
 
          // Write to RAM
-        // 16 bit: RRRRRGGG GGGBBBBB
-        buff[0] = 0;
-        buff[1] = 0;
-        r = ucg->arg.pixel.rgb.color[0]>>(8-5);
-        buff[0] |= r << 3;
-        g = ucg->arg.pixel.rgb.color[1]>>(8-6);
-        buff[0] |= g>>3;
-        buff[1] |= g<<5;
-        b = ucg->arg.pixel.rgb.color[2]>>(8-5);
-        buff[1] |= b&0x1F;
+        set_16bit_color(ucg, buff);
         send_cmd_data(ucg, ST7789_RAMWR, 2, buff);
 
         ucg_com_SetCSLineStatus(ucg, 1);
       }
       return 1;
     case UCG_MSG_DRAW_L90FX:
-      ucg_handle_l90fx(ucg, ucg_dev_st7789_16x240x240);
+      handle_l90fx(ucg);
+      return 1;
+    case UCG_MSG_DRAW_L90SE:
+      ucg_handle_l90se(ucg, ucg_dev_st7789_16x240x240);
       return 1;
   }
 
-  // UCG_MSG_DRAW_L90SE
   // UCG_MSG_SET_CLIP_BOX
   // UCG_MSG_SEND_BUFFER
   return ucg_dev_default_cb(ucg, msg, data);
